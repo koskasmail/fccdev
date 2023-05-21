25c7367c03029f114bd455b037af1d145fe3e05b
kessler yaron <koskasmail@gmail.com>
2023-05-21 13:26:16 +0300

    push() - Shopping List.js

---
+++ fccdev/02_JavaScript/01_Basic_JavaScript/048.Shopping List.js
@@ -0,0 +1,8 @@
+const myList = [];
+  myList.push(["milk", 1]);
+  myList.push(["cheese", 2]);
+  myList.push(["bread", 1]);
+  myList.push(["pizza", 2]);
+  myList.push(["mushroom", 10]);
+
+console.log(myList);
