38423b390112aaa4a6e0aa830f3096f8228338f1
kessler yaron <koskasmail@gmail.com>
2023-05-22 06:38:32 +0300

    global scope variable and function()

---
+++ fccdev/02_JavaScript/01_Basic_JavaScript/052.Global Scope and Functions.js
@@ -0,0 +1,23 @@
+// Declare the myGlobal variable below this line
+let myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal here
+  myGlobal = 5;
+  console.log("myGlobal ===> " + myGlobal);
+}
+
+// Only change code above this line
+
+function fun2() {
+  let output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+fun1();
