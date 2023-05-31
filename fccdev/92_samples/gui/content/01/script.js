8e987c691c2936f15995389997a4a070c168073b
kessler yaron <koskasmail@gmail.com>
2023-05-31 18:28:16 +0300

    ex

---
+++ fccdev/92_samples/gui/content/01/script.js
@@ -0,0 +1,19 @@
+
+
+/**
+ * 
+ */
+function clearAll() {
+  console.log("clearAll")
+  // var fn =  document.getElementById('firstName').innerText = "";
+  var ln =  document.getElementById('lastName').innerText = "";
+  var phe = document.getElementById('phoneNumber').innerText = "";
+
+  var fn = document.getElementById("firstName");
+  fn.innerText = "666";
+}
+
+
+function printOneRecord() {
+  console.log("test");
+}
