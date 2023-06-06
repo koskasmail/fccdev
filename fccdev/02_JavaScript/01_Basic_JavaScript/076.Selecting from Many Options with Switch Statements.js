e77ca3954de4f444e3dd05476b5a82be8f86a181
kessler yaron <koskasmail@gmail.com>
2023-06-06 06:25:47 +0300

    switch() .. case()  - Statements

---
+++ fccdev/02_JavaScript/01_Basic_JavaScript/076.Selecting from Many Options with Switch Statements.js
@@ -0,0 +1,27 @@
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 1:
+    answer = "alpha";
+    console.log("alpha");
+    break;
+  case 2:
+    answer = "beta";
+    console.log("beta");
+    break;
+  case 3:
+    answer = "gamma";
+    console.log("gamma");
+    break;
+  case 4:
+    answer = "delta";
+    console.log("delta");
+    break;
+}
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+caseInSwitch(3);
