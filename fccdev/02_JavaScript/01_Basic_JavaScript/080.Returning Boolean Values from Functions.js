b9a70be696899a4f6e8d69e9fe4b1b4696b49353
kessler yaron <koskasmail@gmail.com>
2023-06-09 17:14:23 +0300

    isLess() isEqual () Returning Boolean Values from Functions

---
+++ fccdev/02_JavaScript/01_Basic_JavaScript/080.Returning Boolean Values from Functions.js
@@ -0,0 +1,14 @@
+function isLess(a, b) {
+  // Only change code below this line
+    return a < b;
+  }
+  // Only change code above this line
+
+  function isEqual(a, b) {
+    return a === b;
+  }
+
+
+console.log("isLess :: " + isLess(10, 15));
+console.log("isLess :: " + isLess(20, 15));
+console.log("isEqual :: " + isEqual(30, 30));
