6681c4a1e38d128fd8d7f427b838a90b2f7a54bc
kessler yaron <koskasmail@gmail.com>
2023-06-09 19:20:58 +0300

    Objects - create an object

---
+++ fccdev/02_JavaScript/01_Basic_JavaScript/083.Build JavaScript Objects.js
@@ -0,0 +1,10 @@
+const myDog = {
+  // Only change code below this line
+  "name":"jay",
+  "legs": 4,
+  "tails": 1,
+  "friends":["cat","wolf"]
+  // Only change code above this line
+};
+
+console.log(myDog);
