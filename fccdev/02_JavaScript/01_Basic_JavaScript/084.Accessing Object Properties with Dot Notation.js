c8ba1557f736c645bea39ed3f79b8982cfb67d4d
kessler yaron <koskasmail@gmail.com>
2023-06-10 05:47:55 +0300

    Objects - get property values from object (key:value)

---
+++ fccdev/02_JavaScript/01_Basic_JavaScript/084.Accessing Object Properties with Dot Notation.js
@@ -0,0 +1,15 @@
+// Setup
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+const hatValue = testObj.hat;      // Change this line
+const shirtValue = testObj.shirt;    // Change this line
+const shoesValue = testObj.shoes;
+
+console.log("hatValue: " + hatValue);
+console.log("shirtValue: " + shirtValue);
+console.log("shoesValue: " + shoesValue);
