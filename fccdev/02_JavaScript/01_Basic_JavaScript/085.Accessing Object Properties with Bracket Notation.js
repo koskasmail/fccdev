0b5e3e92eaa5c62835648ed1aeabda4a576412ce
kessler yaron <koskasmail@gmail.com>
2023-06-10 06:42:53 +0300

    Objects - get property values from object obj1["this key"]

---
+++ fccdev/02_JavaScript/01_Basic_JavaScript/085.Accessing Object Properties with Bracket Notation.js
@@ -0,0 +1,15 @@
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj['an entree'];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+const sideValue = testObj['my side'];
+
+console.log ('entreeValue: ' +  entreeValue);
+console.log ('drinkValue: ' + drinkValue);
+console.log ('sideValue: ' + sideValue);
