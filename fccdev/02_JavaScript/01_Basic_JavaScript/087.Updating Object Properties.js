1c4ff74aa4cd20871d991770fd553d28971eb50b
kessler yaron <koskasmail@gmail.com>
2023-06-15 05:59:53 +0300

    Objects - set/get property values

--- fccdev/02_JavaScript/01_Basic_JavaScript/087.Updating Object Properties.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/087.Updating Object Properties.js
@@ -1,1 +1,13 @@
-change
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["jaron"]
+};
+
+// Only change code below this line
+myDog.name = "Happy Coder";
+console.log("myDog.name: " + myDog.name);
+myDog[name] = "rex";
+console.log("myDog.name: " + myDog.name);
