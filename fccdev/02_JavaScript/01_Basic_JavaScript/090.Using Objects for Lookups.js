b977e229d56c839cd2a2aae929b26dfbf4c0854b
kessler yaron <koskasmail@gmail.com>
2023-06-16 07:30:45 +0300

    Objects for Lookups (key:value pair)

--- fccdev/02_JavaScript/01_Basic_JavaScript/090.Using Objects for Lookups.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/090.Using Objects for Lookups.js
@@ -1,1 +1,24 @@
-change
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+const lookup = {
+  "alpha":"Adams",
+  "bravo":"Boston",
+  "charlie":"Chicago",
+  "delta":"Denver",
+  "echo":"Easy",
+  "foxtrot":"Frank",
+  "":"undefined",
+};
+
+  result = lookup[val];
+  console.log("result: " + result);
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
+phoneticLookup("");
+phoneticLookup("alpha");
