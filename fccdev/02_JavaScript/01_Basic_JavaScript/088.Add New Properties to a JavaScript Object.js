4a7f4ff9e4b6e95005a130ddde736ee1e09bb7d6
kessler yaron <koskasmail@gmail.com>
2023-06-16 06:59:38 +0300

    Objects - add new property values

--- fccdev/02_JavaScript/01_Basic_JavaScript/088.Add New Properties to a JavaScript Object.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/088.Add New Properties to a JavaScript Object.js
@@ -1,1 +1,15 @@
-change
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark = "woof";
+console.log("myDog.bark: " + myDog.bark);
+
+///myDog["hight"] = 30;
+///console.log(myDog["hight"]);
+///myDog['color'] = 'brown';
+///console.log(myDog['color']);
+///console.log(myDog);
