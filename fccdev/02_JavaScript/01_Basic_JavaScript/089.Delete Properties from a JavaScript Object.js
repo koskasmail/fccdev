af8b230ee530a0aeda0c2cb4b925ca9b37e26ca2
kessler yaron <koskasmail@gmail.com>
2023-06-16 07:07:15 +0300

    Objects - delete property values

--- fccdev/02_JavaScript/01_Basic_JavaScript/089.Delete Properties from a JavaScript Object.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/089.Delete Properties from a JavaScript Object.js
@@ -1,1 +1,15 @@
-change
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+
+console.log('myDog.tails: ' + myDog.tails);
+delete myDog.tails;
+
+console.log('myDog.tails: ' + myDog.tails);
