5955cdcaf4195cc1e8bff218400a58daa2300bd7
kessler yaron <koskasmail@gmail.com>
2023-06-17 07:47:49 +0300

    object.hasOwnProperty(property) - Test if Property exist in Objects
    object.hasOwnProperty(property)

--- fccdev/02_JavaScript/01_Basic_JavaScript/091.Testing Objects for Properties.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/091.Testing Objects for Properties.js
@@ -1,1 +1,17 @@
-change
+function checkObj(obj, checkProp) {
+    // Only change code below this line
+    if (obj.hasOwnProperty (checkProp) ) {
+       console.log(obj[checkProp] + ' ==> ' + obj.hasOwnProperty(checkProp));
+      return obj[checkProp];
+    }
+    else {
+        console.log(obj[checkProp] + ' ==> Not Found');
+        return "Not Found";
+    }
+    // Only change code above this line
+  }
+  
+  checkObj({ gift: 'pony', pet: 'kitten', 'bed':'sleigh' }, 'gift');
+  checkObj({ gift: 'pony', pet: 'kitten', 'bed':'sleigh' }, 'set');
+  checkObj({ gift: 'pony', pet: 'kitten', 'bed':'sleigh' }, 'pet');
+  checkObj({city: 'Seattle'}, 'city');
