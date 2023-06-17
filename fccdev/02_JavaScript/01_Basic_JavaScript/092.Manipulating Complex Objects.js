04c9050b23405dd19fa8d7931cbd973ade64cd22
kessler yaron <koskasmail@gmail.com>
2023-06-17 08:33:57 +0300

    Complex Objects - adding new record to object.js

---
+++ fccdev/02_JavaScript/01_Basic_JavaScript/092.Manipulating Complex Objects.js
@@ -0,0 +1,27 @@
+  const myMusic = [
+  {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+    "artist": "Billy Idol",
+    "title": "White Wedding",
+    "release_year": 1982,
+    "formats": [
+      "CD",
+      "8T",
+      "Cassette",
+      "LP"
+    ],
+    "gold": false
+  }
+];
+
+console.table([myMusic]);
