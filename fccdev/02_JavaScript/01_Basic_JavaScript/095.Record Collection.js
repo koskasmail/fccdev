bf9b197aab383cd7dcff10ba70c8828491c18988
kessler yaron <koskasmail@gmail.com>
2023-06-21 12:28:47 +0300

    records object - records, id, tracks, value

--- fccdev/02_JavaScript/01_Basic_JavaScript/095.Record Collection.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/095.Record Collection.js
@@ -1,52 +1,56 @@
+// 095.Record Collection.js
+
 // Setup
 const recordCollection = {
     2548: {
-      albumTitle: 'Slippery When Wet',
-      artist: 'Bon Jovi',
-      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+        albumTitle: 'Slippery When Wet',
+        artist: 'Bon Jovi',
+        tracks: ['Let It Rock', 'You Give Love a Bad Name']
     },
     2468: {
-      albumTitle: '1999',
-      artist: 'Prince',
-      tracks: ['1999', 'Little Red Corvette']
+        albumTitle: '1999',
+        artist: 'Prince',
+        tracks: ['1999', 'Little Red Corvette']
     },
     1245: {
-      artist: 'Robert Palmer',
-      tracks: []
+        artist: 'Robert Palmer',
+        tracks: []
     },
     5439: {
-      albumTitle: 'ABBA Gold'
+        albumTitle: 'ABBA Gold'
     }
-  };
-  
-  // Only change code below this line
-  function updateRecords(records, id, properties, value) {
+};
 
+// Only change code below this line
+function updateRecords(records, id, properties, value) {
 
-    if ( (properties !== "tracks") && (value !== "") ) {
-        records[id][properties] = value;
-    } 
-    else if ((properties === "tracks") && (records[id].hasOwnProperty("tracks") === false) ) {
-        records[id][properties] = value;
-    }
-    else if (properties === "tracks" && value !== "") {
-            records[id][properties].push = value;
-    } 
-    else if (value === "") {
+    if (value === "") {
         delete records[id][properties];
+    } else {
+        /////  if ( (properties !== 'tracks') {}
+        if ((properties !== 'tracks')) {
+            records[id][properties] = value;
+        } else {
+            if (records[id].hasOwnProperty('tracks') === true) {
+                records[id].tracks.push(value);
+            }
+            else { //false
+                records[id].tracks = [];
+                records[id].tracks.push(value);
+            }
+
+        }
     }
-
-
     return records;
-  }
-  
-  console.table(recordCollection);
-  
-  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
-  console.table(recordCollection);
+}
 
-  updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me");
-  console.table(recordCollection);
+console.table(recordCollection);
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+console.table(recordCollection);
 
-  updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
-  console.table(recordCollection);
+updateRecords(recordCollection, 5439, 'tracks', 'Take a Chance on Me');
+console.table(recordCollection);
+
+updateRecords(recordCollection, 1245, 'tracks', 'Addicted to Love')
+console.table(recordCollection);
+
