211cedb97a22d63867c0d693edf3718a2488d785
kessler yaron <koskasmail@gmail.com>
2023-06-23 15:10:38 +0300

    output

--- fccdev/02_JavaScript/01_Basic_JavaScript/095.Record Collection__output.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/095.Record Collection__output.js
@@ -1,1 +1,8 @@
-change
+records[id][prop]...delete: Bon Jovi
+records[id][prop]...delete: undefined
+records[id][prop]...delete: Let It Rock,You Give Love a Bad Name
+records[id][prop]...delete: undefined
+records[id][prop]...delete: Bon Jovi
+records[id][prop]...delete: undefined
+records[id][prop]...delete: Let It Rock,You Give Love a Bad Name
+records[id][prop]...delete: undefined
