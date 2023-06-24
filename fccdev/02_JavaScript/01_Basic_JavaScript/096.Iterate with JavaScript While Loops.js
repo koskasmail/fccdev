0147e48e937532da1dbe18549f52402be84682e3
kessler yaron <koskasmail@gmail.com>
2023-06-24 08:26:47 +0300

    Loops - While() Loops

---
+++ fccdev/02_JavaScript/01_Basic_JavaScript/096.Iterate with JavaScript While Loops.js
@@ -0,0 +1,18 @@
+// Setup
+const myArray = [];
+
+// Only change code below this line
+
+let index = 0;
+while (index < 5) {
+    myArray.push(index);
+    index++;
+}
+
+console.log(myArray);
+
+index = 0;
+while (index < 5) {
+    console.log(index+1 + '> ' + myArray[index]);
+    index++;
+}
