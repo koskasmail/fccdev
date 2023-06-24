c1d38109d3be5f1488400521799be77ad792e569
kessler yaron <koskasmail@gmail.com>
2023-06-24 08:12:00 +0300

    output

--- fccdev/02_JavaScript/01_Basic_JavaScript/095.Record Collection__output.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/095.Record Collection__output.js
@@ -1,8 +1,19 @@
-records[id][prop]...delete: Bon Jovi
-records[id][prop]...delete: undefined
-records[id][prop]...delete: Let It Rock,You Give Love a Bad Name
-records[id][prop]...delete: undefined
-records[id][prop]...delete: Bon Jovi
-records[id][prop]...delete: undefined
-records[id][prop]...delete: Let It Rock,You Give Love a Bad Name
-records[id][prop]...delete: undefined
+//
+///----[output]------------
+//
+// ┌─────────┬─────────────────┬──────────────────────────────────────────────────────────┬─────────────────────┐
+// │ (index) │     artist      │                          tracks                          │     albumTitle      │
+// ├─────────┼─────────────────┼──────────────────────────────────────────────────────────┼─────────────────────┤
+// │  1245   │ 'Robert Palmer' │                  [ 'Addicted to Love' ]                  │      'Riptide'      │
+// │  2468   │    'Prince'     │        [ '1999', 'Little Red Corvette', 'Free' ]         │       '1999'        │
+// │  2548   │    'Shalom'     │                                                          │ 'Slippery When Wet' │
+// │  5439   │     'ABBA'      │ [ 'Take a Chance on Me', 'The Winner', 'Dancing queen' ] │     'ABBA Gold'     │
+// └─────────┴─────────────────┴──────────────────────────────────────────────────────────┴─────────────────────┘
+// ┌─────────┬───────────────────────┐
+// │ (index) │        Values         │
+// ├─────────┼───────────────────────┤
+// │    0    │ 'Take a Chance on Me' │
+// │    1    │     'The Winner'      │
+// │    2    │    'Dancing queen'    │
+// └─────────┴───────────────────────┘
+// 
