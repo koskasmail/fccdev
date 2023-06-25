eba67a31aee23871b65d74904cc8fbfbd151902d
kessler yaron <koskasmail@gmail.com>
2023-06-25 06:20:22 +0300

    Loops - for() Nesting LoopsLoops

--- fccdev/02_JavaScript/01_Basic_JavaScript/101.Nesting For Loops.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/101.Nesting For Loops.js
@@ -1,1 +1,20 @@
-change
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (var a=0; a < arr.length; a++) {
+      for (var b=0; b < arr[a].length; b++) {
+        product = product * arr[a][b];
+        // console.log(product);
+        // console.log(arr[a][b]);   
+      }
+    }
+
+    console.log(product);
+
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1], [2], [3]])
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+  multiplyAll([[5, 1], [0.2, 4, 0.5], [3, 9]])
