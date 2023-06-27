82f5d02361518d222ce2081cc16069633b542674
kessler yaron <koskasmail@gmail.com>
2023-06-27 06:09:32 +0300

    Loops - Do...While() Loops

--- fccdev/02_JavaScript/01_Basic_JavaScript/102.Iterate with JavaScript Do...While Loops.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/102.Iterate with JavaScript Do...While Loops.js
@@ -1,1 +1,12 @@
-change
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  console.log('i:: ' + i);
+  myArray.push(i);
+  i++;
+  console.log('i:: ' + i);
+}
+while (i < 5);
