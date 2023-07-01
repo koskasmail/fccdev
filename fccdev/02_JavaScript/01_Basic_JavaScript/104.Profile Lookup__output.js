b6c982879776166d86ce76281cbb24a46f5bffd5
kessler yaron <koskasmail@gmail.com>
2023-07-01 08:03:43 +0300

    output

--- fccdev/02_JavaScript/01_Basic_JavaScript/104.Profile Lookup__output.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/104.Profile Lookup__output.js
@@ -1,1 +1,10 @@
-change
+//
+///----[output]------------
+//
+// Akira, likes:: Pizza,Coding,Brownie Points
+// Kristian,lastName:: Vos
+// Sherlock, likes:: Intriguing Cases,Violin
+// Harry, likes:: Hogwarts,Magic,Hagrid
+// Bob, number:: No such contact
+// Bob, potato:: No such contact
+// Akira, address:: No such property
