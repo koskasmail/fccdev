1c652242fec92c6eb6ba44dd868efb961f85a586
kessler yaron <koskasmail@gmail.com>
2023-07-09 17:18:41 +0300

    parseInt() - parses a string into an integer (base 2 to 10)

---
+++ fccdev/02_JavaScript/109.Use the parseInt Function with a Radix.js
@@ -0,0 +1,8 @@
+function convertToInteger(str) {
+  
+  return (parseInt(str,2));
+}
+
+console.log(convertToInteger("10") );
+console.log(convertToInteger("100") );
+console.log(convertToInteger("10011") );
