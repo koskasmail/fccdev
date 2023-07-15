7469500d225dfd6f6e2a46dfd5880f96c122ac57
kessler yaron <koskasmail@gmail.com>
2023-07-15 18:36:16 +0300

    Use Multiple Conditional (Ternary) Operators (a > b ? "Yes" : a<b...)

--- fccdev/02_JavaScript/01_Basic_JavaScript/111.Use Multiple Conditional (Ternary) Operators.js
+++ fccdev/02_JavaScript/01_Basic_JavaScript/111.Use Multiple Conditional (Ternary) Operators.js
@@ -1,1 +1,9 @@
-continue
+function checkSign(num) {
+
+   return (num > 0) ? "positive":
+          (num < 0) ? "negative" : "zero";
+}
+
+console.log(checkSign(10));
+console.log(checkSign(-10));
+console.log(checkSign(0));
